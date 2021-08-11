9dc516fb9594b85ce23d5a539ab21accc4522131
David Wischnjak <davidw.software@gmail.com>
2021-08-12 01:19:44 +0200

    added example

---
+++ examples/pmem_simple/pmem_simple.ino
@@ -0,0 +1,21 @@
+#include <LittleFS.h>
+#include <PersistentMemory.h>
+
+void setup(){
+  #ifdef ARDUINO_ARCH_ESP32
+    LittleFS.begin(true);
+  #else
+    LittleFS.begin();
+  #endif
+  Serial.begin(115200);
+
+  PersistentMemory pmem("config", true);
+  int counter = pmem.readInt("counter", 0);
+  pmem.writeInt("counter", counter+1);
+  pmem.commit();
+
+  Serial.printf("This is my %d boot!", counter);
+}
+
+void loop(){
+}
